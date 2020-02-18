c6df17dfc016b101a3b2b761ca442ef37917dad9
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-02-18 10:24:05 +0900

    お気に入り機能追加のため、favoriteという名前のmodelとcontrollerを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   mount_uploader :image, ImageUploader
 
   validates :title,  presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   mount_uploader :image, ImageUploader
-  
+
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :email, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
   has_many :posts
+  has_many :favorites, dependent: :destroy
 end
