07eabcc40e590f440ef3d333b5211bc06c8281a2
kazuyasu aoki <banzai.1yasu@gmail.com>
2020-02-19 10:48:03 +0900

    ユーザー別お気に入りリストの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   root to:'sessions#new'
-  resources :users do
-    member do
-      get :favorites_list
-    end
-  end
-
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
   resources :posts do
     collection do
       post :confirm
+    end
+  end
+  resources :users do
+    member do
+      get :favorite_list
     end
   end
 end
